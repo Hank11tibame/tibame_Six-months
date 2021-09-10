6f19a727b887e2c6cb21290a69f27cfaaa7814a9
Hank11tibame <84449009+Hank11tibame@users.noreply.github.com>
2021-09-10 19:43:55 +0800

    Add files via upload

---
+++ 雜湊函式sha256.py
@@ -0,0 +1,13 @@
+import hashlib
+def create_password(pwd):
+
+
+userid =
+account =
+password = input()
+if userid in account:
+    if account[userid] == create_password(password):
+
+else:
+    print('密碼錯誤')
+print('帳號錯誤')
