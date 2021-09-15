8115b3da91155d137ae1c6092b3ac55648409ed7
Hank11tibame <84449009+Hank11tibame@users.noreply.github.com>
2021-09-15 17:16:30 +0800

    Add files via upload

---
+++ python-list資料結構1.py
@@ -0,0 +1,23 @@
+
+list1 = [[1,2],[3,4],[5,6]]
+
+
+print(list1[0][0])
+print(list1[0][1])
+
+
+print(list1[1][0])
+print(list1[1][1])
+
+print(list1[2][0])
+print(list1[2][1])
+print('====================')
+
+list2 = [[1,2,3],[4,5,6]]
+print(list2[0][0])
+print(list2[0][1])
+print(list2[0][2])
+
+print(list2[1][0])
+print(list2[1][1])
+print(list2[1][2])
